c3e4ddc5bae84c1ec553e2ed5a695bee62483156
rdgasantos <rdgasantos@gmail.com>
2021-01-15 16:55:14 -0300

    Adicionado Angular SPA ao projeto

---
+++ ProAgil-App/src/app/Eventos/Eventos.component.ts
@@ -0,0 +1,29 @@
+import { HttpClient, HttpClientModule } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-Eventos',
+  templateUrl: './Eventos.component.html',
+  styleUrls: ['./Eventos.component.css']
+})
+export class EventosComponent implements OnInit {
+
+  Eventos: any
+
+  constructor(private http: HttpClient) { }
+
+  ngOnInit() {
+    this.getEventos();
+  }
+
+  getEventos() {
+    this.http.get('http://localhost:5000/api/values').subscribe(
+      response => {
+        this.Eventos = response;
+      }, error => {
+        console.log(error);
+      }
+    )
+  }
+
+}
--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # Api em .NET 
 
-This project is for learning ASP.NET
+This project is for learning ASP.NET and Angular
 
-I use .NET CORE Web Api and Entity Framework for developing an API.
+I use .NET CORE Web Api and Entity Framework for developing an API and Angular for inferface.
